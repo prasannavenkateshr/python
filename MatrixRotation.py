fc10d8a91c9d4d66fc041157a8dbc2e697bcb002
Prasan Venky R <prasannavenkateshr@users.noreply.github.com>
2017-03-14 11:55:50 +0530

    MatrixRotation

---
+++ MatrixRotation.py
@@ -0,0 +1,17 @@
+def MatrixLeftRotate(matrix):
+    resultantMatrix = []
+    for i in matrix:
+        resultantMatrix.append(i[::-1])
+    return map(list, zip(*resultantMatrix))
+
+def MatrixRightRotate(matrix):
+    resultantMatrix = []
+    for i in map(list, zip(*matrix)):
+        resultantMatrix.append(i[::-1])
+    return resultantMatrix
+
+def Display(m,n, matrix):
+    for i in xrange(m):
+        for j in xrange(n):
+            print t[i][j],
+        print ""
