5782d4c5afe3b76e8f0b46b11d3f7536a6dae238
Prasan Venky R <prasannavenkateshr@users.noreply.github.com>
2017-04-18 12:34:17 +0530

    Update MatrixRotation.py
    Minor

--- MatrixRotation.py
+++ MatrixRotation.py
@@ -1,13 +1,11 @@
 def MatrixLeftRotate(matrix):
     resultantMatrix = []
-    for i in matrix:
-        resultantMatrix.append(i[::-1])
+    for i in matrix: resultantMatrix.append(i[::-1])
     return map(list, zip(*resultantMatrix))
 
 def MatrixRightRotate(matrix):
     resultantMatrix = []
-    for i in map(list, zip(*matrix)):
-        resultantMatrix.append(i[::-1])
+    for i in map(list, zip(*matrix)): resultantMatrix.append(i[::-1])
     return resultantMatrix
 
 def Display(m,n, matrix):
